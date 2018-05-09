fe2526095a7661bc0c357a567955075f1ded9a86
Sujith Srivardhan Arram <suji.srivardhan@gmail.com>
2018-05-09 12:48:50 -0400

    added user admin related files, and connected databases

---
+++ src/main/java/com/example/myapp/repositories/HelloRepository.java
@@ -0,0 +1,7 @@
+package com.example.myapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.myapp.models.Hello;
+
+public interface HelloRepository extends CrudRepository<Hello, Integer> {}
